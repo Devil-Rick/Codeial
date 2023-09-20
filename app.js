d35273b6f8b851904d64da22eb33b108f560421a
Devil-Rick <saptarshinaruto@gmail.com>
2023-09-21 01:01:03 +0530

    First commit : Setting up directory

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`Error : ${err}`);
+    }
+    console.log(`Server is running on port: ${port}`); //interpolation 
+})
+
+
+
+
