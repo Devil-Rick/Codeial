2930fd9f27ed212bcbdad1f8328d286f356d5e36
Devil-Rick <saptarshinaruto@gmail.com>
2023-10-01 19:47:41 +0530

    manual authentication done

--- controllers/user_contrler.js
+++ controllers/user_contrler.js
@@ -1,18 +1,29 @@
 const User = require('../model/user')
 
-module.exports.profile = (req, res)=>{
-    return res.render('home', {
-        title : "Codeial"
-    })
+module.exports.profile = (req, res) => {
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id)
+        .then((user)=>{
+            if(user){
+                return res.render('user_profile', {
+                    title : "Codeial | " + user.name,
+                    user : user,
+                })
+            }
+            res.redirect('/users/sign-in')
+        })
+    }else{
+        res.redirect('/users/sign-in')
+    }
 }
 
-module.exports.signin = (req, res)=>{
+module.exports.signin = (req, res) => {
     return res.render('user_signIn', {
         title: "Codeial | SignIn"
     })
 };
 
-module.exports.signup = (req, res)=>{
+module.exports.signup = (req, res) => {
     return res.render('user_signUp', {
         title: "Codeial | SignUp"
     })
@@ -20,32 +31,58 @@ module.exports.signup = (req, res)=>{
 
 
 // get the sign up data
-module.exports.newUser = (req, res)=>{
+module.exports.newUser = (req, res) => {
     console.log(req.body);
-    if(req.body.password != req.body.confirm_password){
+    if (req.body.password != req.body.confirm_password) {
         return res.redirect('home')
     }
 
     // searching if the email is available or not
-    User.findOne({email:req.body.email})
-    .then((user)=>{
-        if(!user){
-            // creating the user if user is not present 
-            User.create(req.body)
-            .then((user)=>{
-                console.log(user);
-                res.redirect('/users/sign-in')
-            })
-        }else{
-            res.redirect('back')
-        }
-    }).catch((err)=>{
-        console.log('Error in finding the email id while signing up', err);
-    })
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (!user) {
+                // creating the user if user is not present 
+                User.create(req.body)
+                    .then((user) => {
+                        console.log(user);
+                        res.redirect('/users/sign-in')
+                    })
+            } else {
+                res.redirect('back')
+            }
+        }).catch((err) => {
+            console.log('Error in finding the email id while signing up', err);
+        })
 };
 
 
 // get the sign in Data
-module.exports.session = (req, res)=>{
+module.exports.session = (req, res) => {
+    console.log(req.body);
+    // Find the user in the database
+    User.findOne({ email: req.body.email })
+        .then((user) => {
+            if (user) {
+                if(user.password != req.body.password){
+                    res.redirect('back')
+                }
 
+                res.cookie('user_id', user.id);
+                res.redirect('/users/profile');
+            } else {
+                res.redirect('back')
+            }
+        })
+        .catch((err) =>{
+            console.log('Err in finding user', err);
+        });
 };
+
+module.exports.logout = (req, res) => {
+    console.log('Working');
+    if(req.cookies.user_id){
+        res.clearCookie(req.cookies.user_id);
+        return res.redirect('/')
+    }
+    res.redirect('/users/sign-in')
+}
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const userCntrl = require('../controllers/user_contrler');
 router.get('/profile', userCntrl.profile);
 router.get('/sign-up', userCntrl.signup);
 router.get('/sign-in', userCntrl.signin);
-
 router.post('/new-user', userCntrl.newUser);
-
+router.post('/session', userCntrl.session);
+router.post('/logout', userCntrl.logout);
 module.exports = router;
